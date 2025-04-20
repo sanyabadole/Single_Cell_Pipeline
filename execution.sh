856a92bc69d2fa1e48fdaa64d92b82fe2a85184a
Sanya Badole <144849144+sanyabadole@users.noreply.github.com>
2025-04-20 12:14:40 -0400

    Update execution.sh

--- execution.sh
+++ execution.sh
@@ -8,8 +8,8 @@ show_help() {
     echo "  --process      Look for unprocessed sample_data.h5ad in the data dir. Will output processed_data.h5ad."
     echo "  --help, -h     Display this help message and exit."
     echo
-    echo "If --process is not specified, the script will attempt to run the visualiztion on processed_data.h5ad file."
-    echo "The app requires 'processed_data.h5ad' to exist in the ../data/ directory."
+    echo "If --process is not specified, the script will attempt to run the visualization on processed_data.h5ad file."
+    echo "The app requires 'processed_data.h5ad' to exist in the ./data/ directory."
     echo 
     exit 0
 }
@@ -22,11 +22,11 @@ fi
 # Check if process flag is provided
 if [ "$1" == "--process" ]; then
     # Run the processing pipeline
-    python single_cell_processing_pipeline.py
+    python single_cell_processing.py
 else
     # Check if processed data exists before running the app
-    if [ ! -f "processed_data.h5ad" ]; then
-        echo "Error: 'processed_data.h5ad' not found. Please run with --process flag first."
+    if [ ! -f "./data/processed_data.h5ad" ]; then
+        echo "Error: 'data/processed_data.h5ad' not found. Please run with --process flag first."
         echo 
         exit 1
     fi
